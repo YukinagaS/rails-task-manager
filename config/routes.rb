a43525ee8df3a3dc2bb96885778066185e2a5747
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2024-11-12 16:30:02 +0900

    implemented features

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'tasks', to: 'tasks#index', as: :tasks
+
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  post 'tasks', to: 'tasks#create'
+
+  patch 'tasks/:id', to: 'tasks#update'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
